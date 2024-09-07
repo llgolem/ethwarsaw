b4471d6958f0a07d1961b9198916f1a2696bf9b4
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-09-07 20:08:03 +0200

    feat: added token for deployment with the llm

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+}
