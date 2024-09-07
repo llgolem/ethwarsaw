c77e6e4cfb751abe672095fdc5cb3205542bcea8
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-09-07 08:20:28 +0200

    feat: added private signer

--- app/src/env.mjs
+++ app/src/env.mjs
@@ -5,10 +5,13 @@ export const env = createEnv({
   server: {},
   client: {
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().min(1),
+    NEXT_PUBLIC_REQUEST_NETWORK_SIGNER: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+      NEXT_PUBLIC_REQUEST_NETWORK_SIGNER:
+      process.env.NEXT_PUBLIC_REQUEST_NETWORK_SIGNER,
   },
   skipValidation:
     !!process.env.SKIP_ENV_VALIDATION &&
--- app/src/hooks/request-network/client.ts
+++ app/src/hooks/request-network/client.ts
@@ -1,13 +1,21 @@
-import { RequestNetwork } from "@requestnetwork/request-client.js";
-import { Web3SignatureProvider } from "@requestnetwork/web3-signature";
+import { RequestNetwork } from "@requestnetwork/request-client.js"
+import { EthereumPrivateKeySignatureProvider } from "@requestnetwork/epk-signature"
+import { Web3SignatureProvider } from "@requestnetwork/web3-signature"
 
-export const getRequestClient = () => {
-  const web3SignatureProvider = new Web3SignatureProvider(window.ethereum);
+export const getRequestClient = (privateKey?: string) => {
+  let signatureProvider
+
+  if (privateKey)
+    signatureProvider = new EthereumPrivateKeySignatureProvider({
+      method: EthereumPrivateKeySignatureProvider.getMethod(),
+      privateKey,
+    })
+  else signatureProvider = new Web3SignatureProvider(window.ethereum)
 
   return new RequestNetwork({
     nodeConnectionConfig: {
       baseURL: "https://sepolia.gateway.request.network/",
     },
-    signatureProvider: web3SignatureProvider,
-  });
-};
+    signatureProvider,
+  })
+}
