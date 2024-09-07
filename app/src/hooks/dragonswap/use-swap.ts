1743c8293a67e1ba2f785d3ae6d2f46f068b4cf1
ahmedhamedaly <ahmedhamedaly19@gmail.com>
2024-09-07 14:17:11 +0200

    feat: useSwap hook for dragonswap

---
+++ app/src/hooks/dragonswap/use-swap.ts
@@ -0,0 +1,49 @@
+import { toast } from "sonner"
+import { writeContract } from "wagmi/actions"
+import { config } from "@/lib/wagmi"
+import { useMutation } from "@tanstack/react-query"
+import { Address, parseUnits } from "viem"
+import { ROUTER_ADDRESS, ROUTER_ABI } from "@/lib/constants"
+
+type SwapParams = {
+  amountIn: string
+  amountOutMin: string
+  path: Address[]
+  to: Address
+  deadline: bigint
+}
+
+export function useSwap() {
+  const swapExactTokensForTokens = useMutation({
+    mutationFn: async ({ amountIn, amountOutMin, path, to, deadline }: SwapParams) => {
+      const result = await writeContract(config, {
+        address: ROUTER_ADDRESS,
+        abi: ROUTER_ABI,
+        functionName: "swapExactTokensForTokens",
+        args: [
+          parseUnits(amountIn, 18),
+          parseUnits(amountOutMin, 18),
+          path,
+          to,
+          deadline
+        ],
+      })
+
+      if (!result) throw new Error("Failed to swap tokens")
+
+      return result
+    },
+    onError: (error: Error) => {
+      console.error("Swap error:", error)
+      if (error.message.includes("User rejected the request"))
+        return toast.error("User denied transaction signature.")
+      toast.error(error.message)
+    },
+    onSuccess: (result) => {
+      toast.success("Swap executed successfully!")
+      console.log("Swap result:", result)
+    },
+  })
+
+  return { swapExactTokensForTokens }
+}
--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -1,1 +1,40 @@
-export const REQUEST_NETWORK_ADDRESS = "0x80245CB7Dd7f6Ad18503dFbD2f269fb3B9322341"
+export const REQUEST_NETWORK_ADDRESS =
+  "0x80245CB7Dd7f6Ad18503dFbD2f269fb3B9322341"
+
+export const ROUTER_ADDRESS = "0x4c0b142FA93fF118474f69568953a2966f31a627"
+export const ROUTER_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amountIn",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "amountOutMin",
+        type: "uint256",
+      },
+      {
+        internalType: "address[]",
+        name: "path",
+        type: "address[]",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+    ],
+    name: "swapExactTokensForTokens",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "amountOut",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+]
